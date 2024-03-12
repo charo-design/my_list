d727f3b2afd0145e33bfca9522716e08a3584fca
Ann Charity <charitykaroki1@gmail.com>
2024-03-12 23:29:47 +0300

    first commit

---
+++ myList.py
@@ -0,0 +1,15 @@
+my_List=[]
+print(my_List)
+my_List=[10,20,30,40]
+print(my_List)
+my_List.insert(2, 15)
+print(my_List)
+another_list=[50,60,70]
+my_List.extend(another_list)
+print(my_List)
+my_List.pop()
+print(my_List)
+my_List.sort()
+print(my_List)
+index_of_30=my_List.index(30)
+print("The index of 30 is:",index_of_30)
